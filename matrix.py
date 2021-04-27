ed475f2f78a425f6ec4bcf46b4b2181080fb454f
elizabethsotomayor <elizabethrsotomayor@gmail.com>
2021-04-27 14:34:23 -0500

    refactor code

--- matrix.py
+++ matrix.py
@@ -2,15 +2,14 @@ from typing import List
 
 
 class Matrix:
-    def __init__(self, matrix_string: str):
+    def __init__(self, matrix_string: str) -> None:
         """Instantiate a Matrix object with given values separated by a newline."""
-        self.matrix = [i.split() for i in matrix_string.splitlines()]
-        self.nums = [[int(y) for y in x] for x in self.matrix]
+        self.matrix = [[int(item) for item in line.split()] for line in matrix_string.splitlines()]
 
     def row(self, index: int) -> List[int]:
         """Return the values in the row of a Matrix object from the index of a row."""
-        return self.nums[index - 1]
+        return self.matrix[index - 1]
 
     def column(self, index: int) -> List[int]:
         """Return the values in the column of a Matrix object from the index of a column."""
-        return [x[index - 1] for x in self.nums]
+        return [x[index - 1] for x in self.matrix]
